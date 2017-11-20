daf6349bb6b2a4ef6407bfab75d7611015e22f70
buttslapper <jfranny92@aol.com>
2017-11-20 11:50:24 -0800

    Initial commit - new crimes are now stored in a database and display properly - pg 285

---
+++ app/src/main/java/com/example/johnkaehler/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.johnkaehler.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by John Kaehler on 4/20/2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/example/johnkaehler/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,24 @@
+package com.example.johnkaehler.criminalintent.database;
+
+/**
+ * Created by John Kaehler on 11/16/2017.
+ */
+
+public class CrimeDbSchema {
+
+    public static final class CrimeTable{
+        public static final String NAME = "crimes";
+
+
+        public static final class Cols{
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+
+
+
+
+}
