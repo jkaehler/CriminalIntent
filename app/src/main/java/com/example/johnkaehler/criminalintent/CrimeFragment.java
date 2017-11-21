e662bb7a549fcfd496a1c93f9c91fc613e7796ae
buttslapper <jfranny92@aol.com>
2017-11-21 14:41:18 -0800

    Got references to new views in CrimeFragment

--- app/src/main/java/com/example/johnkaehler/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/johnkaehler/criminalintent/CrimeFragment.java
@@ -19,6 +19,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Date;
 import java.util.UUID;
@@ -43,6 +45,8 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
     private Button mReportButton;
     private Button mSuspectButton;
+    private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
 
     public static CrimeFragment newInstance(UUID crimeId){
         Bundle args = new Bundle();
@@ -207,6 +211,9 @@ public class CrimeFragment extends Fragment {
                 PackageManager.MATCH_DEFAULT_ONLY) == null){
             mSuspectButton.setEnabled(false);
         }
+
+        mPhotoButton = (ImageButton)v.findViewById(R.id.crime_camera);
+        mPhotoView = (ImageView)v.findViewById(R.id.crime_photo);                                   
 
         return v;
     }
