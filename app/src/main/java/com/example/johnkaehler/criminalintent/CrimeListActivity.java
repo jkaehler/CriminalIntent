97039fb800247fc32b3f06d771adf3cdbf021c89
buttslapper <jfranny92@aol.com>
2017-12-05 21:34:16 -0800

    Crime detail now shows up in correct pane when crime is clicked on tablet.

--- app/src/main/java/com/example/johnkaehler/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/johnkaehler/criminalintent/CrimeListActivity.java
@@ -1,12 +1,13 @@
 package com.example.johnkaehler.criminalintent;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by John Kaehler on 4/20/2017.
  */
 
-public class CrimeListActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity implements CrimeListFragment.Callbacks{
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
@@ -14,6 +15,22 @@ public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected int getLayoutResId(){
-        return R.layout.activity_twopane;
+        return R.layout.activity_masterdetail;
+    }
+
+    @Override
+    public void onCrimeSelected(Crime crime) {
+
+        if(findViewById(R.id.detail_fragment_container) == null){
+            Intent intent = CrimePagerActivity.newIntent(this, crime.getId());
+        }
+        else {
+            Fragment newDetail = CrimeFragment.newInstance(crime.getId());
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail_fragment_container, newDetail)
+                    .commit();
+        }
+
     }
 }
